e98b5b9bcdd4dbf10cbe8c3c18edf4b06a0676e2
YannikSue <y.suedbeck@yahoo.de>
2024-11-13 11:34:09 +0100

    initial commit

---
+++ src/main/java/org/schmuh/quisine/QuisineApplication.java
@@ -0,0 +1,13 @@
+package org.schmuh.quisine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuisineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuisineApplication.class, args);
+	}
+
+}
