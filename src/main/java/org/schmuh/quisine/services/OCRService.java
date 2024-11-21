2b491d8e06446820f63dbaa3711eed35b1573c2c
Simon <szy.matuszkiewicz@gmail.com>
2024-11-21 17:13:41 +0100

    yannik zwingt mich

--- src/main/java/org/schmuh/quisine/services/OCRService.java
+++ src/main/java/org/schmuh/quisine/services/OCRService.java
@@ -3,9 +3,17 @@ package org.schmuh.quisine.services;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 @NoArgsConstructor
 @AllArgsConstructor
 public class OCRService {
     @Value("${ocrPath}")
     private String ocrPath;
+
+    public String GetTextFromPicture(){
+        Tesseract tesseract
+    }
 }
